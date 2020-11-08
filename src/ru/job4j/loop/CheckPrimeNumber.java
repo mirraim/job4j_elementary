556276a6961352208a6b7a8fc79f7b88fdafee47
Maria Pankratova <mirraim@gmail.com>
2020-11-08 17:38:45 +0500

    5.5. Простое число [#399290]

---
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+
+    public static boolean check(int number) {
+        boolean isPrime = true;
+        if (number <= 1) {
+            isPrime = false;
+        }
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        return isPrime;
+    }
+}
