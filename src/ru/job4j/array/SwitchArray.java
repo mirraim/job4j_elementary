4ccc9e0f454a50a1b9c4a31fcd53eb86d0a11f9a
Maria Pankratova <mirraim@gmail.com>
2020-11-09 17:13:42 +0500

    6.4.1.1. Обход массива с последнего элемента [#399224]

--- src/ru/job4j/array/SwitchArray.java
+++ src/ru/job4j/array/SwitchArray.java
@@ -18,8 +18,11 @@ public class SwitchArray {
     public static void main(String[] args) {
         int[] nums = new int[] {1, 2, 3, 4, 5, 6};
         int[] rsl = swapBorder(nums);
-        for (int i : rsl) {
-            System.out.println(i);
+        for (int i = 0; i < rsl.length; i++) {
+            int index = rsl.length - 1 - i;
+            if (index % 2 == 0) {
+                System.out.println("index = " + index + ", value = " + rsl[index]);
+            }
         }
     }
 }
