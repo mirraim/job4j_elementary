e1274501d385d38816b4911931cc6f3146a889c8
Maria Pankratova <mirraim@gmail.com>
2020-12-02 22:32:26 +0500

    3. Интерфейс Input [#399341]

---
+++ src/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void ride() {
+
+    }
+
+    @Override
+    public void passengers(int number) {
+
+    }
+
+    @Override
+    public int fill(int quantity) {
+        return quantity;
+    }
+}
