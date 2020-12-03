ee06b341ace5444d42b3baebdd7ebc99e8248d24
Maria Pankratova <mirraim@gmail.com>
2020-12-03 19:49:49 +0500

    3. Интерфейс Input [#399341]

--- src/ru/job4j/poly/Bus.java
+++ src/ru/job4j/poly/Bus.java
@@ -3,16 +3,17 @@ package ru.job4j.poly;
 public class Bus implements Transport {
     @Override
     public void ride() {
-
+        System.out.println("Bus rides");
     }
 
     @Override
     public void passengers(int number) {
-
+        System.out.println("There are " + number + " passengers on the bus");
     }
 
     @Override
     public int fill(int quantity) {
+        System.out.println("Bus is filled");
         return quantity;
     }
 }
