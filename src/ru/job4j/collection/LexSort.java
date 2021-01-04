4904dc210a4acff625f008fa15d3af8a4506355a
Maria Pankratova <mirraim@gmail.com>
2021-01-04 12:31:17 +0500

    4. Сортировка номера [#399313]

---
+++ src/ru/job4j/collection/LexSort.java
@@ -0,0 +1,12 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        int leftNum = Integer.parseInt(left.substring(0, left.indexOf(".")));
+        int rightNum = Integer.parseInt(right.substring(0, right.indexOf(".")));
+        return Integer.compare(leftNum, rightNum);
+    }
+}
