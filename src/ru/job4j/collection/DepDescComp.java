d6ebb88a430d4ead11932a776033284d26e27791
Maria Pankratova <mirraim@gmail.com>
2021-01-10 15:41:39 +0500

    2. Отсортировать департаменты [#399308] - исправлена сортировка по убыванию

--- src/ru/job4j/collection/DepDescComp.java
+++ src/ru/job4j/collection/DepDescComp.java
@@ -5,22 +5,12 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        int size = Math.min(o1.length(), o2.length());
-        int start = 0;
-        while (o1.charAt(start) != '/') {
-            if (o1.charAt(start) != o2.charAt(start)) {
-                return Integer.compare(o2.charAt(start), o1.charAt(start));
-            }
-            start++;
-            if (start == size) {
-                break;
-            }
+        String first = o1.split("/")[0];
+        String second = o2.split("/")[0];
+        int rsl = second.compareTo(first);
+        if (rsl == 0) {
+            rsl = o1.compareTo(o2);
         }
-        for (int i = start; i < size; i++) {
-            if (o1.charAt(i) != o2.charAt(i)) {
-                return Integer.compare(o1.charAt(i), o2.charAt(i));
-            }
-        }
-        return Integer.compare(o1.length(), o2.length());
+        return rsl;
     }
 }
