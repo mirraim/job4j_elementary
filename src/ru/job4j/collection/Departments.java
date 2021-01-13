42d468df5cc126a80aaf69999badeaa7a932262c
Maria Pankratova <mirraim@gmail.com>
2021-01-13 20:47:26 +0500

    Добавлен Javadoc в класс Departments

--- src/ru/job4j/collection/Departments.java
+++ src/ru/job4j/collection/Departments.java
@@ -3,6 +3,15 @@ package ru.job4j.collection;
 import java.util.*;
 
 public class Departments {
+    /**
+     *  Метод заполняет пропущенные департаменты
+     *  Каждое название разбивается на подстроки
+     *  Потом собирается снова,
+     *  причем при каждом новом добавлении подстроки происходит запись строки во временный Set
+     *  После прохождения всех итераций Set преобразуется в List
+     * @param deps на вход принимается список строк
+     * @return возвращается список строк, в котором устранены пропуски
+     */
     public static List<String> fillGaps(List<String> deps) {
         Set<String> tmp = new LinkedHashSet<>();
         for (String value : deps) {
